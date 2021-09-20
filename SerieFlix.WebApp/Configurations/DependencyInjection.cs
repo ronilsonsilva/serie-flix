380addb9d0baa1011b16b8ca2641f8e0fbd47a7b
R S Silva <rs.ronilsonsilva@outlook.com.br>
2021-09-19 22:33:37 -0300

    alterador presentation

---
+++ SerieFlix.WebApp/Configurations/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SerieFlix.WebApp.Configurations
+{
+    public static class DependencyInjection
+    {
+        public static void Resolve(IServiceCollection services)
+        {
+
+        }
+    }
+}
